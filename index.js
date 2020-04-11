d52641f18952fc131c76e9cf018abc056531c2d2
DaniDPX <tech@danidpx.com>
2020-04-11 15:50:54 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function randomNoGenerator(min, max) {
+    if(typeof(max) !== 'number' && typeof(min) !== 'number') {
+      min = 0;  max = 1;
+    }
+   return (Math.random() * (max-min)) + min;
+  }
+  
+
+  //This line should be written at end of your project
+  module.exports = randomNoGenerator;
+  
